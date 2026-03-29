b9ff7c2aa6550272cddad52aab77e9d8fe2ab9ee
ANA RODRÍGUEZ DE VERA MARTÍNEZ <Ana.Rodriguez24@alu.uclm.es>
2026-03-29 12:46:09 +0200

    Implentación composite

--- contenedor.py
+++ contenedor.py
@@ -3,9 +3,11 @@ from elemento_mapa import ElementoMapa
 
 class Contenedor(ElementoMapa):
     def __init__(self):
+        super().__init__()
         self.hijos = []
 
     def agregarHijo(self, hijo):
+        hijo.padre = self
         self.hijos.append(hijo)
 
     def recorrer_hijos(self):
--- elemento_mapa.py
+++ elemento_mapa.py
@@ -1,3 +1,6 @@
 class ElementoMapa:
+    def __init__(self):
+        self.padre = None
+
     def entrar(self, alguien=None):
         raise NotImplementedError("Este metodo debe redefinirse")
